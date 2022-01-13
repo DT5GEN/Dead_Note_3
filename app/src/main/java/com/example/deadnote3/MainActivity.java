5cc1134b176b908b66ec34b8dfe10f3ada717dad
DT5Gen <marinovod@gmail.com>
2022-01-14 00:14:20 +0300

    тест учебного видео паралленьного потока

--- app/src/main/java/com/example/deadnote3/MainActivity.java
+++ app/src/main/java/com/example/deadnote3/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.deadnote3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = " HappY MainActivity >> ";
+private Button fragmentStartButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        Log.d(TAG, "onCreate() called with: savedInstanceState = [" + savedInstanceState + "]");
+        fragmentStartButton = findViewById(R.id.start_first_fragment);
+        fragmentStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+}}
