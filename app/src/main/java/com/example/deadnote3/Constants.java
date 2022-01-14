8774f29ea1174ecd9229f57372a8bb78f1411637
DT5Gen <marinovod@gmail.com>
2022-01-15 00:18:22 +0300

    Возвращение везультата в активити

---
+++ app/src/main/java/com/example/deadnote3/Constants.java
@@ -0,0 +1,5 @@
+package com.example.deadnote3;
+
+public interface Constants {
+    String YOUR_ACCOUNT = "YOUR_ACCOUNT";
+}
--- app/src/main/java/com/example/deadnote3/MainActivity.java
+++ app/src/main/java/com/example/deadnote3/MainActivity.java
@@ -2,19 +2,78 @@ package com.example.deadnote3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity  {
-
-
+public class MainActivity extends AppCompatActivity implements Constants{
+    private static final int REQUEST_CODE_SETTING_ACTIVITY = 99;
+    private EditText txtName;
+    private Account account;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        account = new Account();
+        initView();
+
+    }
+
+    private void initView() {
+
+        Button btnGreetings = findViewById(R.id.btnGreetings);
+        txtName = findViewById(R.id.textName);
+        final TextView txtGreetings = findViewById(R.id.textHello);
+        btnGreetings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = txtName.getText().toString();
+                String sayHello = getString(R.string.say_hello) + name;
+                txtGreetings.setText(sayHello);
             }
+        });
+
+        Button btnSettings = findViewById(R.id.btnSettings);
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Чтобы стартовать активити, надо подготовить интент
+                // В данном случае это будет явный интент, поскольку здесь передаётся класс активити
+                Intent runSettings = new Intent(MainActivity.this, SettingsActivity.class);
+                populateAccount();
+                // Передача данных через интент
+                runSettings.putExtra(YOUR_ACCOUNT, account);
+                startActivityForResult(runSettings, REQUEST_CODE_SETTING_ACTIVITY);
+            }
+        });
+
+
+    }
+
+    private void populateAccount(){
+        account.setName(txtName.getText().toString());
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode != REQUEST_CODE_SETTING_ACTIVITY) {
+            super.onActivityResult(requestCode, resultCode, data);
+            return;
         }
+
+        if (resultCode == RESULT_OK){
+            account = data.getParcelableExtra(YOUR_ACCOUNT);
+            populateView();
+        }
+    }
+
+    private void populateView(){
+        txtName.setText(account.getName());
+    }
+
+}
